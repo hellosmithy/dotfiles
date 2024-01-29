043cd09f60bcbc8cd0689cf3401475fff2b9af0f
Ben Smith <ben@hellosmithy.com>
2024-01-29 11:08:58 +0900

    replace exa (depracated) -> lsd

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -1,2 +1,3 @@
-alias ll='exa -lag --header'
+alias ls='lsd'
+alias ll='ls -la'
 alias tmux-last='tmux attach-session -t "$(tmux list-sessions -F '#{session_name}:#{session_last_attached}' | sort -rk2,2 | cut -d: -f1 | head -n 1)"'
