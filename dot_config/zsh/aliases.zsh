2e7d27b1df53ecd5160822665c61611e7f038490
Ben Smith <ben@hellosmithy.com>
2024-01-29 11:38:06 +0900

    grep alias

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -1,3 +1,11 @@
+# ls
 alias ls='lsd'
 alias ll='ls -la'
+
+# grep
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# tmux
 alias tmux-last='tmux attach-session -t "$(tmux list-sessions -F '#{session_name}:#{session_last_attached}' | sort -rk2,2 | cut -d: -f1 | head -n 1)"'
