f3de957f5025743d747b8e13e97eb38b40492047
Ben Smith <ben@hellosmithy.com>
2024-01-29 11:44:55 +0900

    safe mv/cp/rm aliases

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -7,5 +7,10 @@ alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
 
+# safe mv, cp, rm
+alias mv='mv -i'
+alias cp='cp -i'
+alias rm='rm -i'
+
 # tmux
 alias tmux-last='tmux attach-session -t "$(tmux list-sessions -F '#{session_name}:#{session_last_attached}' | sort -rk2,2 | cut -d: -f1 | head -n 1)"'
