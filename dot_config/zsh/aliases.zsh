7005cf736a0dfd195edcc3eb3b65e5bbd7af06b5
Ben Smith <ben@hellosmithy.com>
2024-02-23 16:08:44 +0900

    add lazyvim

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -14,3 +14,6 @@ alias rm='rm -i'
 
 # tmux
 alias tmux-last='tmux attach-session -t "$(tmux list-sessions -F '#{session_name}:#{session_last_attached}' | sort -rk2,2 | cut -d: -f1 | head -n 1)"'
+
+# neovim
+alias n='nvim'
