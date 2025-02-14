d5b9303e20a79b21d22eed79f893de655e877696
Ben Smith <ben@hellosmithy.com>
2025-02-14 10:41:38 +0900

    replace asdf with mise

--- dot_config/zsh/plugins/asdf.zsh
+++
@@ -1,6 +0,0 @@
-. "$(brew --prefix asdf)/libexec/asdf.sh"
-
-# append completions to fpath
-fpath=(${ASDF_DIR}/completions $fpath)
-# initialise completions with ZSH's compinit
-autoload -Uz compinit && compinit
