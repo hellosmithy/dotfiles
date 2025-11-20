7857dab6147ded96c158ae8cead6f94056a16bb4
Ben Smith <ben@hellosmithy.com>
2025-11-20 14:55:17 +0000

    remove rye crumbs

--- scripts/run_once_rye_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if ! command -v rye &> /dev/null; then
-  echo "Rye is not installed. Please install it manually before running this script."
-  exit 1
-fi
-
-rye self install --yes --no-modify-path
