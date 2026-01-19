416a0862a0554c7f87363f74e17d2373c9f10da3
Ben Smith <ben@hellosmithy.com>
2026-01-19 10:26:34 +0000

    rename kitty tabs

--- dot_config/zsh/plugins/ghq-fzf.zsh
+++ dot_config/zsh/plugins/ghq-fzf.zsh
@@ -4,6 +4,12 @@ export GHQ_ROOT="$HOME/projects"
 function fzf-src () {
   local selected_dir=$(ghq list --full-path | fzf -1 -e)
   if [ -n "$selected_dir" ]; then
+    # Rename kitty tab to project name before cd
+    if [ "$TERM" = "xterm-kitty" ]; then
+      local project_name="${selected_dir:h:t}/${selected_dir:t}"
+      kitty @ set-tab-title "$project_name"
+    fi
+
     BUFFER="cd ${selected_dir}"
     zle accept-line
   fi
