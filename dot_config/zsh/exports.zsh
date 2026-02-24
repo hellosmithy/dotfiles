81d2427c638d22256377de24828f16b4772a5c41
Ben Smith <ben@hellosmithy.com>
2026-02-24 14:21:56 +0000

    obsidian cli

--- dot_config/zsh/exports.zsh
+++ dot_config/zsh/exports.zsh
@@ -2,7 +2,10 @@ HISTFILE=~/.zsh_history
 HISTSIZE=1000000
 SAVEHIST=1000000
 
+# Local scripts
 export PATH="$HOME/.local/bin":$PATH
+# Obsidian CLI
+export PATH="$PATH:/Applications/Obsidian.app/Contents/MacOS"
 export MANPAGER='nvim +Man!'
 export MANWIDTH=999
 
